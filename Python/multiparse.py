83702520e5bdd63e059f309486202ecb74dc1680
fearnley.l <fearnley.l@wehi.edu.au>
2021-04-04 19:03:44 +1000

    Clean up of comments, unused variables, imports etc.

--- Python/multiparse.py
+++ Python/multiparse.py
@@ -26,8 +26,8 @@ def create_output_dir(path_list, clobber):
         if os.path.exists(path):
             if not clobber:
                 print("ERROR: An output file path exists at " + path)
-                print("Exiting - re-run with the --clobber flag to ignore existing "
-                      "files")
+                print("Exiting - re-run with the --clobber flag to ignore "
+                      "existing files")
                 import sys
                 sys.exit(-1)
             else:
@@ -42,7 +42,6 @@ def read_perread_file(queue, read_summary_path):
     experiment_id = path.parent.name
     max_length = 0
     collation_dict = {}
-    done_set = set()
     total_count = 0
     successful_run = False
     try:
@@ -71,7 +70,7 @@ def read_perread_file(queue, read_summary_path):
                     max_length = max(max_length, int(str_tuple[4]))
         successful_run = True
     except EOFError as ex:
-        print("Note: ", read_summary_path, " has a malformed end-of-file." )
+        print("Note: ", read_summary_path, " has a malformed end-of-file.")
     if total_count == 0 or not successful_run:
         # print("ERROR " + read_summary_path)
         queue.put((experiment_id, 0, {}))
@@ -80,9 +79,10 @@ def read_perread_file(queue, read_summary_path):
         print(read_summary_path + "\t" + str(total_count))
         for key in collation_dict:
             for idx in collation_dict[key]:
-                collation_dict[key][idx] = 1.0*(collation_dict[key][idx])/total_count
+                collation_dict[key][idx] = (1.0 * collation_dict[key][idx]) / total_count
     queue.put((read_summary_path, max_length, collation_dict))
     return
+
 
 def write_file(queue, motif_csv_out_path, sample_csv_out_path, manifest_path):
     seen_motifs = set()
@@ -112,20 +112,17 @@ def write_file(queue, motif_csv_out_path, sample_csv_out_path, manifest_path):
         if max_len == 0:
             print("ERROR: Sample parse error - Skipping exp " + experiment_id)
             continue
-        output_list = []
-        length_dict = defaultdict(int)
-        min_length = 1000
-        max_length = -1000
         # Fix this up.
-        with open(sample_csv_out_path + experiment_id + ".csv", 'wt') as sample_file_handle:
+        with open(sample_csv_out_path + experiment_id + ".csv",
+                  'wt') as sample_file_handle:
             sample_file_handle.write("motif,mlen")
-            for i in range(1, max_len+1):
-                sample_file_handle.write(","+str(i))
+            for i in range(1, max_len + 1):
+                sample_file_handle.write("," + str(i))
             sample_file_handle.write("\n")
             for key in collation_dict:
                 key_len = str(len(key))
-                sample_file_handle.write(key +"," +str(len(key.strip())))
-                folder_root = motif_csv_out_path+key_len+"mers/"
+                sample_file_handle.write(key + "," + str(len(key.strip())))
+                folder_root = motif_csv_out_path + key_len + "mers/"
                 if not os.path.exists(folder_root):
                     os.makedirs(folder_root)
                 with open(folder_root + key + ".csv", 'at') as csv_file_handle:
@@ -135,39 +132,42 @@ def write_file(queue, motif_csv_out_path, sample_csv_out_path, manifest_path):
                             csv_file_handle.write("," + str(i))
                         csv_file_handle.write("\n")
                         seen_motifs.add(key)
-                    csv_file_handle.write(experiment_id + "," + key + ","
-                                          + str(len(key)) + "," + str(max_len) +","+group)
+                    csv_file_handle.write(experiment_id + "," + key + "," + str(len(key)) + "," + str(max_len) + "," + group)
                     key_dict = collation_dict[key]
-                    for count_idx in range(1, max_len+1):
+                    for count_idx in range(1, max_len + 1):
                         key_idx = str(count_idx)
                         if key_idx in key_dict:
                             csv_file_handle.write("," + str(key_dict[key_idx]))
-                            sample_file_handle.write(","+str(key_dict[key_idx]))
+                            sample_file_handle.write(
+                                "," + str(key_dict[key_idx]))
                         else:
                             csv_file_handle.write(",")
                             sample_file_handle.write(",")
                     csv_file_handle.write("\n")
                     sample_file_handle.write("\n")
-                    min_length = min(min_length, len(key))
         queue.task_done()
     return
+
 
 def chunks(lst, n):
     """Yield successive n-sized chunks from lst."""
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
 
+
 if __name__ == "__main__":
     import argparse
+
     parser = argparse.ArgumentParser()
     paths_group = parser.add_argument_group(title="File paths")
     paths_group.add_argument("--input", action="store", dest="input_path",
-                        help="Input path; should be a directory containing "
-                             "one or more superSTR runs.")
-    paths_group.add_argument("-m", "--manifest", dest="manifest_path", default=None,
+                             help="Input path; should be a directory containing "
+                                  "one or more superSTR runs.")
+    paths_group.add_argument("-m", "--manifest", dest="manifest_path",
+                             default=None,
                              help="Manifest path")
     paths_group.add_argument("--output", action="store", dest="output_path",
-                        help="Output path for files.")
+                             help="Output path for files.")
     optional_arguments = parser.add_argument_group(title="Optional arguments")
     optional_arguments.add_argument("--clobber", action="store_true",
                                     dest="clobber", default=False,
@@ -176,9 +176,9 @@ if __name__ == "__main__":
                                     dest="read_length", default=150,
                                     help="Manually set read length (default 150nt).")
     optional_arguments.add_argument("-@", "--threads", action="store",
-                            dest="threadcount", default=4,
-                            help="Number of threads for multiparser execution"
-                                 " (default=4)")
+                                    dest="threadcount", default=4,
+                                    help="Number of threads for multiparser execution"
+                                         " (default=4)")
     # Default to help message if no arguments provided.
     if len(sys.argv) == 1:
         parser.print_help(sys.stderr)
@@ -192,7 +192,7 @@ if __name__ == "__main__":
     sample_csv_out_path = output_root_path + "/samples/"
     manifest_path = os.path.abspath(args.manifest_path)
     # Create/clobber output directories as required.
-    create_output_dir([motif_csv_out_path,sample_csv_out_path], args.clobber)
+    create_output_dir([motif_csv_out_path, sample_csv_out_path], args.clobber)
     # Processing: if summarise flag set, input should be processed first.
     pool = Pool(thread_count)
     m = multiprocessing.Manager()
@@ -208,12 +208,15 @@ if __name__ == "__main__":
     else:
         root_path = os.path.abspath(args.input_path)
         filelist = glob.glob(root_path + "*/per_read.txt.gz")
-    p = multiprocessing.Process(target=write_file, args=(queue, motif_csv_out_path, sample_csv_out_path, manifest_path))
+    for file in filelist:
+        print(file)
+    p = multiprocessing.Process(target=write_file, args=(
+        queue, motif_csv_out_path, sample_csv_out_path, manifest_path))
     p.start()
-    # split filelist in 10 pieces:
     inputargs = [(queue, os.path.abspath(file)) for file in filelist]
     pool.starmap(read_perread_file, inputargs)
     pool.close()
     pool.join()
     queue.put("DONE")
     p.join()
+
