6c7a3b034dc341589836d1eb0cf21582945c1764
lfearnley <lfearnley@users.noreply.github.com>
2021-07-08 16:18:43 +1000

    Update install_script.sh
    Shift htslib directory to within superSTR folder.

--- examples/install_script.sh
+++ examples/install_script.sh
@@ -3,6 +3,9 @@ set -euxo pipefail
 
 ##This script assumes that wget, tar, make, git and cmake are installed on your system.
 
+##superSTR
+git clone https://github.com/bahlolab/superSTR
+cd superSTR
 BASEDIR=$(pwd)
 ##superSTR dependency - HTSLIB 1.11
 wget https://github.com/samtools/htslib/releases/download/1.11/htslib-1.11.tar.bz2
@@ -13,9 +16,8 @@ cd htslib-1.11/
 export HTSLIB_ROOT=$BASEDIR/htslib
 make
 make install
-##superSTR
 cd $BASEDIR
-git clone https://github.com/bahlolab/superSTR
-cd superSTR/C/
+rm -rf htslib-1.11/
+cd $BASEDIR/C/
 cmake .
 make
